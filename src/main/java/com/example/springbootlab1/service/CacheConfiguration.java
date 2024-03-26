ef83c44015f1b91da5508751c426f2aea9278002
Adam Moran <adammoran974@gmail.com>
2024-03-26 18:39:42 +0900

    Lab3: Limmiting cache memory

--- src/main/java/com/example/springbootlab1/service/CacheConfiguration.java
+++ src/main/java/com/example/springbootlab1/service/CacheConfiguration.java
@@ -1,13 +1,13 @@
 package com.example.springbootlab1.service;
 
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CacheEvict;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.TimeUnit;
 
 
 @Configuration
@@ -15,14 +15,10 @@ import org.springframework.scheduling.annotation.Scheduled;
 @EnableScheduling
 public class CacheConfiguration {
     @Bean
-    public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("sunInfoCache");
-    }
-
-    @CacheEvict(cacheNames = "sunInfoCache", allEntries = true)
-    @Scheduled(fixedRate = 600000) //10 минут ожидания
-    public void clearCache(){
-        //Метод пустой так как для аннотации @CacheEvict,
-        //которая просто очищает кеш, аннотация не нужна
+    public CaffeineCacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("sunInfoCache");
+        cacheManager.setCaffeine(Caffeine.newBuilder().maximumSize(1).expireAfterWrite(10, TimeUnit.SECONDS).
+                evictionListener((key, value, cause) -> System.out.println("Evicted: " + key + ", Cause: " + cause.toString())));
+        return cacheManager;
     }
 }
