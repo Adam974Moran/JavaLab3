b44438f0bd4c336995426036a13a9fbbf4c37d8e
Adam Moran <adammoran974@gmail.com>
2024-04-20 22:28:33 +0300

    Tests 81% Fixed Issues 2

--- src/test/java/com/example/springbootlab1/service/UrlGeneratorTest.java
+++ src/test/java/com/example/springbootlab1/service/UrlGeneratorTest.java
@@ -2,63 +2,23 @@ package com.example.springbootlab1.service;
 
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UrlGeneratorTest {
 
   @SneakyThrows
-  @Test
-  void testGenerateNewUrl_AllParameters() {
-    String lat = "51.5085";
-    String lng = "-0.1257";
-    String date = "2023-04-01";
-    String formatted = "2";
-    String expectedUrl = "https://api.sunrise-sunset.org/json?lat=51.5085&lng=-0.1257&date=2023-04-01&formatted=2";
-
+  @ParameterizedTest
+  @CsvSource({
+      "51.5085, -0.1257, 2023-04-01, 2, https://api.sunrise-sunset.org/json?lat=51.5085&lng=-0.1257&date=2023-04-01&formatted=2",
+      "51.5085, -0.1257, 2023-04-01, null, https://api.sunrise-sunset.org/json?lat=51.5085&lng=-0.1257&date=2023-04-01",
+      "51.5085, -0.1257, null, 2, https://api.sunrise-sunset.org/json?lat=51.5085&lng=-0.1257&formatted=2",
+      "51.5085, -0.1257, null, null, https://api.sunrise-sunset.org/json?lat=51.5085&lng=-0.1257"
+  })
+  void testGenerateNewUrl(String lat, String lng, String date, String formatted, String expectedUrl) {
     String actualUrl = UrlGenerator.generateNewUrl(lat, lng, date, formatted);
-
-    assertEquals(expectedUrl, actualUrl);
-  }
-
-  @SneakyThrows
-  @Test
-  void testGenerateNewUrl_DateOnly() {
-    String lat = "51.5085";
-    String lng = "-0.1257";
-    String date = "2023-04-01";
-    String formatted = "null";
-    String expectedUrl = "https://api.sunrise-sunset.org/json?lat=51.5085&lng=-0.1257&date=2023-04-01";
-
-    String actualUrl = UrlGenerator.generateNewUrl(lat, lng, date, formatted);
-
-    assertEquals(expectedUrl, actualUrl);
-  }
-
-  @SneakyThrows
-  @Test
-  void testGenerateNewUrl_FormattedOnly() {
-    String lat = "51.5085";
-    String lng = "-0.1257";
-    String date = "null";
-    String formatted = "2";
-    String expectedUrl = "https://api.sunrise-sunset.org/json?lat=51.5085&lng=-0.1257&formatted=2";
-
-    String actualUrl = UrlGenerator.generateNewUrl(lat, lng, date, formatted);
-
-    assertEquals(expectedUrl, actualUrl);
-  }
-
-  @SneakyThrows
-  @Test
-  void testGenerateNewUrl_NoOptionalParameters() {
-    String lat = "51.5085";
-    String lng = "-0.1257";
-    String date = "null";
-    String formatted = "null";
-    String expectedUrl = "https://api.sunrise-sunset.org/json?lat=51.5085&lng=-0.1257";
-
-    String actualUrl = UrlGenerator.generateNewUrl(lat, lng, date, formatted);
-
     assertEquals(expectedUrl, actualUrl);
   }
 
