e263fb7499223e6a8b5ebe15e217df7c24068952
Adam Moran <adammoran974@gmail.com>
2024-05-01 23:11:07 +0300

    LAB 6_1

--- src/main/java/com/example/springbootlab1/SpringBootLab1.java
+++ src/main/java/com/example/springbootlab1/SpringBootLab1.java
@@ -3,7 +3,6 @@ package com.example.springbootlab1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * The type Spring boot lab 1.
